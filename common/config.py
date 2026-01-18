cc412fa3a85a623a842562da77fb8ccadfbb5160
sota hayashi <so1ta6@icloud.com>
2026-01-18 09:07:25 +0900

    Modify the file name

--- common/config.py
+++ common/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # デフォルトのデータパスはJSONに差し替え（必要に応じて変更してください）
-DATA_PATH = Path("data_online_experiment/616033a44ba802b7e18daaa9.json")
-SUMMARY_PATH = Path("./hmm_summary.csv")
+DATA_PATH = Path("data_online_experiment/5e6473fbc15dbe1f71eea95b.json")
+SUMMARY_PATH = Path("./hmm_normalized_summary.csv")
 
 PRACTICE_ROWS = 32
 ROWS_PER_SESSION = 96
