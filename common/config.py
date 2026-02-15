27eeaa93b19846bb86998ca2f39dcf4cecb8b6ad
sota hayashi <so1ta6@icloud.com>
2026-02-15 11:35:03 +0900

    Modify a bit

--- common/config.py
+++ common/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # デフォルトのデータパスはJSONに差し替え（必要に応じて変更してください）
-DATA_PATH = Path("data_online_experiment/65fd4ff0fac6ac4525f54b88.json")
-SUMMARY_PATH = Path("./hmm_summary/hmm_summary_normalized_ver2.0.csv")
+DATA_PATH = Path("data_online_experiment/666408427db5e38fe0ea1736.json")
+SUMMARY_PATH = Path("./hmm_summary/hmm_summary_normalized_decisive_prob.csv")
 
 PRACTICE_ROWS = 32
 ROWS_PER_SESSION = 96
