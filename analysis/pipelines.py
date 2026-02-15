90abdcc0dcbcdbc269928103aacff639179cff6f
sota hayashi <so1ta6@icloud.com>
2026-02-15 11:33:14 +0900

    Modify and Call functions

--- analysis/pipelines.py
+++ analysis/pipelines.py
@@ -1,11 +1,16 @@
 from pathlib import Path
-from common.config import SUMMARY_PATH
-from io_data.load import load_all_concatenated
+from common.config import DATA_PATH, SUMMARY_PATH
+from io_data.load import load_and_prepare, load_all_concatenated
 from analysis.pipelines_behavior import run_behavior, get_subjects_by_behavior_data
 from analysis.pipelines_hmm import get_subjects_by_hmm_category
 
 
 def run_default():
+    # 0) 確認したい参加者データはここで参照
+    # _, df_learning, df_awareness = load_and_prepare(DATA_PATH)
+    # plot_logistic_regression_per_subject(df_learning)
+    # plot_logistic_regression_per_subject(df_awareness)
+
     # 1) データを一度だけロード
     all_data_practice, all_data_learning, all_data_awareness = load_all_concatenated(
         Path("data_online_experiment"),
@@ -20,13 +25,13 @@ def run_default():
     group5 = "on-off-cycling"
     categories = [group1, group2, group3, group4, group5]
 
-    subjects_hmm, _ = get_subjects_by_hmm_category(SUMMARY_PATH, categories=categories)
+    # subjects_hmm, _ = get_subjects_by_hmm_category(SUMMARY_PATH, categories=categories)
 
     # 3) 行動基準で被験者を抽出
-    subjects_behavior = get_subjects_by_behavior_data(all_data_learning, threshold=2)
+    subjects_behavior_on, subjects_behavior_off = get_subjects_by_behavior_data(all_data_learning, threshold=2)
 
     # 4) 両方の条件を満たす被験者を抽出（AND）
-    subjects = subjects_behavior
+    subjects = subjects_behavior_on + subjects_behavior_off
 
     # 5) フィルタして行動解析
     if subjects:
@@ -34,4 +39,4 @@ def run_default():
             Path("data_online_experiment"),
             subjects_include=subjects
         )
-    run_behavior(all_data_learning, all_data_awareness)
+    run_behavior(all_data_learning, all_data_awareness, subjects_behavior_on, subjects_behavior_off)
--- analysis/pipelines_behavior.py
+++ analysis/pipelines_behavior.py
@@ -8,12 +8,22 @@ from features.behavior import (
 )
 from stats.metrics import (
     t_test_count_target_choice_between_periods,
+    t_test_count_target_choice_between_subjects,
     t_test_learning_rate_from_switch_probs,
 )
 from viz.plots import (
     plot_hist_with_lognormal_fit,
     plot_exploit_target_prob_by_switch,
+    plot_logistic_regression_per_subject,
+    plot_MW_vs_reward_across_subjects,
 
+)
+from common.config import (
+    SUMMARY_PATH,
+    DATA_PATH,
+)
+from io_data.load import (
+    load_and_prepare,
 )
 
 
@@ -26,33 +36,43 @@ def get_subjects_by_behavior_data(
     例: task_relevant_choice_count >= threshold を満たす被験者。
     """
     task_relevant_choice_df = compute_task_relevant_choice_rate_subjects(all_data_learning)
-    return task_relevant_choice_df.loc[
-        task_relevant_choice_df["task_relevant_choice_count"] >= threshold,
+    subjects_behavior_1 = task_relevant_choice_df.loc[
+        task_relevant_choice_df["task_irrelevant_choice_count"] < threshold - 1,
+        "subject"
+    ].tolist()
+    subjects_behavior_2 = task_relevant_choice_df.loc[
+        task_relevant_choice_df["task_irrelevant_choice_count"] >= threshold,
         "subject"
     ].tolist()
 
+    return subjects_behavior_1, subjects_behavior_2
 
-def run_behavior(all_data_learning, all_data_awareness=None):
+
+def run_behavior(all_data_learning, all_data_awareness=None, subjects_behavior_on=None, subjects_behavior_off=None):
     task_relevant_choice_df = compute_task_relevant_choice_rate_subjects(all_data_learning)
     target_choice_df = compute_target_choice_rate_subjects(all_data_learning)
     target_choice_prob_by_switch_df = compute_target_choice_prob_by_task_irrelevant_switch(all_data_learning)
-    plot_exploit_target_prob_by_switch(target_choice_prob_by_switch_df)
-    print(t_test_learning_rate_from_switch_probs(target_choice_prob_by_switch_df))
+    # plot_exploit_target_prob_by_switch(target_choice_prob_by_switch_df)
+    # print(t_test_learning_rate_from_switch_probs(target_choice_prob_by_switch_df))
 
     print("\nTask relevant choice rate per subject:")
     print(task_relevant_choice_df)
-    print(
-        f"\nMean task relevant choice rate across subjects: "
-        f"{task_relevant_choice_df['task_relevant_choice_rate'].mean()}"
-    )
 
     results = t_test_count_target_choice_between_periods(all_data_learning)
     print("\nT-test: Target choice rate between early and late periods")
-    print(f"T-statistic: {results['t_stat']}, P-value: {results['p_value']}")
+    print(f"n = {results['n']}, T-statistic: {results['t_stat']}, P-value: {results['p_value']}")
     print(
         f"Mean (all): {results['mean_total']}, "
         f"Mean (first half): {results['mean_first_half']}, "
         f"Mean (second half): {results['mean_second_half']}"
+    )
+
+    results = t_test_count_target_choice_between_subjects(all_data_learning, subjects_behavior_on, subjects_behavior_off)
+    print("\nT-test: Target choice rate between subjects with high vs low task-relevant choice")
+    print(f"n_group1 = {results['n_group1']}, n_group2 = {results['n_group2']}, T-statistic: {results['t_stat']}, P-value: {results['p_value']}")
+    print(
+        f"Mean (high task-relevant choice): {results['mean_delta_group1']}, "
+        f"Mean (low task-relevant choice): {results['mean_delta_group2']}"
     )
 
     # Example: AE variance histogram
@@ -64,6 +84,9 @@ def run_behavior(all_data_learning, all_data_awareness=None):
     #     xlabel="Angular Error Variance",
     #     title="Histogram of Angular Error Variances Across Subjects"
     # )
+
+    # MWと報酬（平均ターゲット選択確率）の関係をプロット
+    # plot_MW_vs_reward_across_subjects(all_data_learning)
 
     return {
         "all_data_learning": all_data_learning,
